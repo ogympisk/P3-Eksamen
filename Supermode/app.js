3a80cbd8cdf114cea03abfbda601180028293bef
ogympisk <kristofferdennis05@gmail.com>
2023-04-26 18:42:28 +0200

    Adding files

---
+++ Supermode/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes')
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://kristoffde:DftbDRCcOuk89m95@supermodedata.bavrwd8.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/veileder', (req, res) => res.render('veileder'));
+app.use(authRoutes);
