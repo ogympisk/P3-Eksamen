7c73622ceb424f636241b22922dba9343a75d38c
ogympisk <kristofferdennis05@gmail.com>
2023-04-28 13:34:27 +0200

    fixing port

--- Supermode/app.js
+++ Supermode/app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 // database connection
 const dbURI = 'mongodb+srv://kristoffde:DftbDRCcOuk89m95@supermodedata.bavrwd8.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(3000))
+  .then((result) => app.listen(80))
   .catch((err) => console.log(err));
 
 // routes
